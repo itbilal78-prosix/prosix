a90d045b443b6066ad39d815d9b8d6cc895fbbde
Muhammad Bilal <itbilal78@gmail.com>
2026-05-05 14:00:18 +0500

    update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -7,20 +7,22 @@ use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     */
-protected function redirectTo($request)
-{
-    if ($request->expectsJson() || $request->is('api/*')) {
+    protected function redirectTo($request)
+    {
+        if ($request->expectsJson() || $request->is('api/*')) {
+            return null;
+        }
+
+        $host = $request->getHost();
+
+        if (str_contains($host, 'admin.') || str_contains($host, 'customizer.')) {
+            return route('admin.login');
+        }
+
+        if ($request->is('admin') || $request->is('admin/*')) {
+            return route('admin.login');
+        }
+
         return null;
     }
-
-    if ($request->is('admin') || $request->is('admin/*')) {
-        return route('admin.login');
-    }
-
-    return null;
-}
-
 }
